61b801f9c329b250a6047ced1329b6bc7a354863
Tux <77551844+Pancho-Developer@users.noreply.github.com>
2022-02-15 12:13:47 -0300

    Add header

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,3 +1,35 @@
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const services = document.getElementById('services');
+const contact = document.getElementById('contact');
+
+home.addEventListener('click', () => {
+    home.classList.add('active');
+    about.classList.remove('active');
+    services.classList.remove('active');
+    contact.classList.remove('active');
+})
+about.addEventListener('click', () => {
+    about.classList.add('active');
+    home.classList.remove('active');
+    services.classList.remove('active');
+    contact.classList.remove('active');
+})
+
+services.addEventListener('click', () => {
+    services.classList.add('active');
+    home.classList.remove('active');
+    about.classList.remove('active');
+    contact.classList.remove('active');
+})
+
+contact.addEventListener('click', () => {
+    contact.classList.add('active');
+    home.classList.remove('active');
+    about.classList.remove('active')
+    services.classList.remove('active');
+})
+
 const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('active');
